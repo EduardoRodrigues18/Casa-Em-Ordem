3f233c4823ba2a40d306ba6145e44fb89749d45e
Eduardo R. dos Santos <91218824+EduardoRodrigues18@users.noreply.github.com>
2025-06-21 16:58:58 -0300

    First Commit
    Primeiro commit para start do trabalho, iniciado com todas as dependências instaladas

---
+++ Projeto/casa-em-ordem/src/Routes/AuthStack.js
@@ -0,0 +1,16 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from '../screens/LoginScreen';
+import RegisterScreen from '../screens/RegisterScreen';
+import AppTabs from './AppTabs';
+
+const Stack = createNativeStackNavigator();
+
+export default function AuthStack() {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Register" component={RegisterScreen} />
+      <Stack.Screen name="Home" component={AppTabs} />
+    </Stack.Navigator>
+  );
+}
